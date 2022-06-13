80268874335f03024ef3269f3d5bf265dfd43d2f
srihitha <srihithatsr@gmail.com>
2022-06-13 17:53:03 +0530

    Updated Code

--- app/src/main/java/com/sritadip/chatapp/UserAdapter.kt
+++ app/src/main/java/com/sritadip/chatapp/UserAdapter.kt
@@ -1,11 +1,13 @@
 package com.sritadip.chatapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context: Context, val userList:ArrayList<User>):RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
@@ -22,11 +24,18 @@ class UserAdapter(val context: Context, val userList:ArrayList<User>):RecyclerVi
         val currentUser=userList[position]
         holder.textName.text=currentUser.name
 
+        holder.itemView.setOnClickListener{
+            val intent=Intent(context,ChatActivity::class.java)
+
+            intent.putExtra("name",currentUser.name)
+            intent.putExtra("uid",currentUser.uid)
+            context.startActivity(intent)
+        }
+
     }
 
     override fun getItemCount(): Int {
         return userList.size
-        TODO("Not yet implemented")
     }
 
 
